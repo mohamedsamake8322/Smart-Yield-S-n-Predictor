d470838a4a2be7095ceadd24a5777a8f4cf837b3
Mohamed SAMAKE <mohamedsamake8322@gmail.com>
2025-05-16 19:16:54 +0300

    ds

--- hash_passwords.py
+++ hash_passwords.py
@@ -1,7 +1,27 @@
 import streamlit_authenticator as stauth
+import json
 
+names = ["Mohamed", "SAMAKE"]
+usernames = ["mohamed", "samake"]
 passwords = ["78772652Moh#", "78772652Moh@"]
 
-hashed_passwords = stauth.Hasher().hash_passwords(passwords)
+# Construire le dictionnaire credentials avec mots de passe en clair
+credentials = {
+    "usernames": {
+        usernames[i]: {
+            "name": names[i],
+            "password": passwords[i]
+        } for i in range(len(usernames))
+    }
+}
 
-print(hashed_passwords)
+# Hasher les mots de passe dans credentials
+hashed_credentials = stauth.Hasher().hash_passwords(credentials)
+
+# Afficher le dictionnaire hashé
+print("Hashed credentials :")
+print(json.dumps(hashed_credentials, indent=4))
+
+# Sauvegarder dans un fichier JSON
+with open("hashed_credentials.json", "w") as f:
+    json.dump(hashed_credentials, f, indent=4)
