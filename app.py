c0a248d859806c98367d2a566997bd7274de58ac
Mohamed SAMAKE <mohamedsamake8322@gmail.com>
2025-05-21 20:53:59 +0200

    nhb

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import requests
 import joblib
 import logging
 import sklearn
-# 🔹 Vérifier si Streamlit reconnaît les secrets
-st.secrets.load_if_needed()  # 🔥 Recharge les secrets si nécessaire
 
 st.write("🔍 Vérification secrets.toml rechargé")
 st.write("DB_NAME:", st.secrets["connections.postgresql"]["database"])
