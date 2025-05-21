d5a86f0ffc2c7f9eb866597de9fb17777d80e417
Mohamed SAMAKE <mohamedsamake8322@gmail.com>
2025-05-21 10:11:05 +0300

    nhg

--- auth.py
+++ auth.py
@@ -2,8 +2,6 @@ import psycopg2
 import bcrypt
 from dotenv import load_dotenv
 import os
-from auth import verify_password
-
 # 🔹 Assure le bon chargement du fichier `.env`
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")  # 🔍 Recherche `.env` dans le dossier du script
 load_dotenv(dotenv_path)  # 🔥 Charge les variables depuis `.env`
